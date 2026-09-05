56d23e91f67c7007b2b304beb9c1240a418ef686
B1gMaks <maxim.golubets2006@gmail.com>
2026-09-05 14:06:53 +0300

    init.py

---
+++ init.py
@@ -0,0 +1,9 @@
+from .stats import StatisticsEngine
+from .histograms import HistogramGenerator
+from .reports import ReportGenerator
+
+__all__ = [
+    "StatisticsEngine",
+    "HistogramGenerator",
+    "ReportGenerator",
+]
